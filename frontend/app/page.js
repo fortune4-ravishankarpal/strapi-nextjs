f0cc9ba4e4d09ee5888c20fe46deb1a1d31172c5
RamDoijadFortune4 <rameshwar.d@fortune4.in>
2024-07-08 18:56:45 +0530

    fetch home data form api

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,8 +1,21 @@
-import { Button } from "@/components/ui/button"
-export default function Home() {
+import axios from "axios";
+
+
+async function getHomePageData() {
+  return await axios.get("http://localhost:1337/api/home").then(res => res.data.data.attributes)
+}
+
+export default async function Home() {
+  let response = await getHomePageData()
+  let { title, description } = response
   return (
     <>
-      <Button variant="outline"> button is working </Button>
+      <h1 className="text-xl">
+        {title}
+      </h1>
+      <h4>
+        {description}
+      </h4>
     </>
   );
 }
