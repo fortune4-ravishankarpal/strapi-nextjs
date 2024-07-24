1ffe1d78d51b5829ac9ca2fd472da763f52891a0
fortune4-ravishankarpal <ravishankar.pal@fortune4.in>
2024-07-24 16:58:59 +0530

    order detail work in pending

---
+++ backend/src/api/orderdetails/services/orderdetails.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/**
+ * orderdetails service
+ */
+
+module.exports = () => ({});
--- backend/src/api/product/routes/product.js
+++ backend/src/api/product/routes/product.js
@@ -7,3 +7,17 @@
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::product.product');
+
+module.exports = {
+    routes: [
+        {
+            method: 'GET',
+            path: '/products',
+            handler: 'product.findMany',
+            config: {
+                policies: [],
+                middlewares: [],
+            },
+        }
+    ]
+}
